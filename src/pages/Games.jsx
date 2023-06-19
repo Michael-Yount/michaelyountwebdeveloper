fc0c38658165fd75759d582734c3de5eb6dff6e3
Michael A. Yount <cheshirekatsmile101@gmail.com>
2023-06-18 21:13:22 -0400

    removed images

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -3,12 +3,12 @@ import bubbleAttack from '../bubbleAttack.png'
 function Games() {
     console.log('games');
     return (
-        <div className="container">
+        <div className="game-container">
             <h1 className='page-title'>Games</h1>
-        <div class="card">
+        <div className="card">
         <img src={bubbleAttack} className="card-img-top bubbleAttack" alt="..."/>
-        <div class="card-body">
-          <p class="card-text">Video game development has many facets, the work is never ending and there are more tasks that seem to build every day. Adding features to a game can be very challenging and time consuming, but these are all learning experiences.</p>
+        <div className="card-body">
+          <p className="card-text">Video game development has many facets, the work is never ending and there are more tasks that seem to build every day. Adding features to a game can be very challenging and time consuming, but these are all learning experiences.</p>
         </div>
       </div>
         </div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,14 @@
+import { Document } from "react-pdf";
 
 function Resume() {
     console.log('resume');
     return (
-        <>
+        <div>
             <h1 className="page-title">Resume</h1>
-        </>
+            <Document file="../FullStackResume_endRevision_PDF (1).pdf">
+                
+            </Document>
+        </div>
     )
 }
 export default Resume;
