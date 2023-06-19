ce78d320197b7e5c4e7891cbeda4228bddf59f5f
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-06-19 14:50:50 -0400

    added resume

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -4,9 +4,62 @@ function Resume() {
         console.log('resume');
     
     return (
-        <div>
+        <div className="container">
             <h1 className="page-title">Resume</h1>
-                
+               <p className="resume">
+                  Michael A. Yount
+                  <br/> 
+                    <br/>cheshirekatsmile101@gmail.com | 585 441 4762 | Lehighton PA.
+                    <br/>www.linkedin.com/in/michael-yount  | https://michaelyountwebportfolio.netlify.app/
+                    <br/>
+                    <br/>
+                    SKILLS: 
+                    <br/>
+                    <br/>MERN Stack (MongoDB, ExpressJS, React, NodeJS) AWS, Netlify, Vercel, JavaScript, CSS, HTML, SASS, Animation, Unity 3d, Photoshop, Blender, Gimp.
+                    <br/> 
+                    <br/>EDUCATION 
+                    <br/>
+                    <br/>MIT xPRO, Professional Certificate in Coding: Full Stack Development with MERN       Sept. 2023 
+                    <br/> 
+                    <br/>				                  
+                    PROJECTS
+                    <br/> 
+                    <br/>Kitchen Prep, a kitchen team building app.
+                    <br/>Full Stack Developer 
+                    <br/>●	Tech Stack: modular JavaScript, HTML5, CSS, SASS , Photoshop. 
+                    <br/>●	Used complex SASS to build a front-end application with JavaScript functionality.
+                    <br/>●	Productivity app, designed to increase workflow in a commercial kitchen. It has several 
+                    <br/>features that make kitchen workflow more manageable. 
+                    <br/>●	Future features will include: recipe API, and weather reporting for customer probability. 
+                    <br/>
+                    <br/>
+
+                    Blue Honey/Soap by Sea`
+                    <br/>Front End Developer
+                    <br/>●	Tech Stack: modular vanilla JavaScript, HTML, CSS, SASS.
+                    <br/>●	Commerce website used to sell soap and reach many customers across the web.
+                    <br/>●	Future features will include a monthly subscription for soap and email, mailing list. 
+                    <br/>
+                    <br/>Freelance Projects
+                    <br/>Game environment Design. Lead UI.
+                    <br/>•	Q-bots. Worked in a team to develop a sample game environment. 
+                    <br/>
+                    <br/>2d game design and Animation.
+                    <br/>•	“Stray” video game. Worked with a small team and created several game environment and 
+                    <br/>character animations.
+                    <br/>
+                    EXPERIENCE 
+                    <br/>
+                    <br/>
+                    SURV Restaurant						     	March 22 to present
+                    <br/>Chef 
+                    <br/>●	Organize kitchen scheduling and train several employees to work efficiently and within the <br/>scope of the company’s standards.
+                    <br/>●	Analyze inventory and order spreadsheets to effectively impact company spending.
+                    <br/>●	Train multiple employees on food safety and handling as well as proper cooking techniques.
+ 
+ 
+
+                </p> 
 
         </div>
     )
--- src/pages/WebApps.jsx
+++ src/pages/WebApps.jsx
@@ -1,6 +1,6 @@
 import bankApp from '../BadBAnkThumbnail.png';
 import dadJoke from '../dadJokes.png';
-
+import kitchenprep from '../kitchenprep.png'
 
 function WebApps() {
     console.log('web apps');
@@ -9,7 +9,7 @@ function WebApps() {
             <h1 className="page-title">Apps</h1>
         <div className="container-apps col">
             <div className="card">
-                <img src={bankApp} className="img-fluid" alt="..."/>
+                <img src={bankApp} className="img-fluid pictures" alt="..."/>
                 <div className="card-body">
                 <h5 className="card-title">My Bad Bank</h5>
                  <p className="card-text">This is a project used to display the use of Global state and routing functionality of a single page application.
@@ -20,16 +20,28 @@ function WebApps() {
         </div>
         <div className="container-apps col">
             <div className="card">
-                <img src={dadJoke} className="img-fluid" alt="..."/>
+                <img src={dadJoke} className="img-fluid pictures" alt="..."/>
                 <div className="card-body">
                 <h5 className="card-title">Dad Joke Generator</h5>
                  <p className="card-text">This is an API fetch app. In this app I utilize axios fetch and bring data into the react app. Future additions will include Chuck Norris Jokes.
-                 <br/> Click here to check out htis app.</p>
+                 <br/> Click here to check out this app.</p>
                  <a href='https://michael-yount.github.io/TheAbsolutlyWorseDadJokes/' type='button' className='btn'>Dad Jokes</a>
                 </div>
             </div>
+           </div> 
+        <div className="container-apps col">
+            <div className="card">
+                <img src={kitchenprep} className="img-fluid pictures" alt="..."/>
+                <div className="card-body">
+                <h5 className="card-title">Kitchen Prep App</h5>
+                 <p className="card-text">This is a productivity and team building app generally used for comercial kitchens.
+                 <br/> Click here to check out this app.</p>
+                 <a href='https://kitchenprepapp.netlify.app/' type='button' className='btn'>Kitchen App</a>
+                </div>
+            </div>
         </div>
-    </div>  
+    </div>
+    
     )
 }
 export default WebApps;
