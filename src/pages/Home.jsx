e8f441ebf24ffae1644b40ef012ba8cebd62d3fc
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-06-23 09:55:46 -0400

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import lavaBoss from '../bossLevel.png';
 import apps from '../BadBAnkThumbnail.png';
 import art from '../mugshot.png'
+import mypic from '../MYlogo.png'
+
  
  function Home() {
     console.log('home');
@@ -9,7 +11,7 @@ import art from '../mugshot.png'
             <div className="title-container">
                 <div className="cool-phrase">
                     <h1 className='name-title'>Michael<br/>Yount</h1>
-                    <p>I strive to develop excellent user experiences.</p>
+                    <p>I strive to develop excellent user experiences for everyone.</p>
                 </div>
             </div>
             <div className="row">
@@ -26,13 +28,25 @@ import art from '../mugshot.png'
                             <p className="card-text">Digital art is truly expressive, I enjoy many different approaches to digital art.</p>
                             <a href="#/Art" className="btn btn-outline-success">Art</a>
                 </div>
+            <div className='row'>   
                 <div className="col">
                     <img src={lavaBoss} className="card-img-top img-fluid" alt="..."/>
                             <h5 className="card-title">Games</h5>
                             <p className="card-text">Here is where my digital art comes alive in these games that I have developed.</p>
                             <a href="#/Games" className="btn btn-outline-success">Games</a>
                 </div>
+                <div className="col">
+                    <img src={lavaBoss} className="card-img-top img-fluid" alt="..."/>
+                            <h5 className="card-title">Games</h5>
+                            <p className="card-text">Here is where my digital art comes alive in these games that I have developed.</p>
+                            <a href="#/Games" className="btn btn-outline-success">Games</a>
+                </div>
+            </div>     
             </div>
+    <div className='footer'>
+    <img src={mypic} className="img-fluid" alt="..."/>
+
+    </div>
         </div>
     )
  }
