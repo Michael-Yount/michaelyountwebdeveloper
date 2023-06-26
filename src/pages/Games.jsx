5f3e9a13d1e243d8d1ee2b4917783915580ce33d
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-06-25 22:42:42 -0400

    fixed links

--- src/pages/Games.jsx
+++ src/pages/Games.jsx
@@ -17,32 +17,32 @@ function Games() {
                     <img src={chicken} className="card-img-top img-fluid" alt="..."/>
                             <h5 className="card-title">Why Did the Chicken Cross the Universe?</h5>
                             <p className="card-text">This game is a very large project.</p>
-                            <a href="https://michaelyount.itch.io/why-did-the-chicken-cross-the-universe" className="btn btn-outline-success">Chicken</a>
+                            <a href="https://michaelyount.itch.io/why-did-the-chicken-cross-the-universe" className="btn btn-outline-success" target='_blank' rel='noopener noreferrer'>Chicken</a>
           </div>
           <div className='col'>
                     <img src={wind} className="card-img-top img-fluid" alt="..."/>
                             <h5 className="card-title">Wind Whirl</h5>
                             <p className="card-text">Follow a seed throught he world and help it survive.</p>
-                            <a href="https://michaelyount.itch.io/wind-whirl" className="btn btn-outline-success">Seed</a>
+                            <a href="https://michaelyount.itch.io/wind-whirl" className="btn btn-outline-success" target='_blank' rel='noopener noreferrer'>Seed</a>
             </div>  
         <div className='row'>  
           <div className='col'>
                     <img src={pill} className="card-img-top img-fluid" alt="..."/>
                             <h5 className="card-title">Deeper into the Mind.</h5>
                             <p className="card-text">This was my entry into ludum dare 48. This game was created in 72 hours with a small team.</p>
-                            <a href="https://michaelyount.itch.io/deeper-into-the-mind" className="btn btn-outline-success">Ludum48</a>
+                            <a href="https://michaelyount.itch.io/deeper-into-the-mind" className="btn btn-outline-success" target='_blank' rel='noopener noreferrer'>Ludum48</a>
             </div>  
             <div className='col'>
                     <img src={join} className="card-img-top img-fluid" alt="..."/>
                             <h5 className="card-title">Join Logic</h5>
                             <p className="card-text">This was my entry into another game jam, where a small team completed this in 48 hours.</p>
-                            <a href="https://grantyoumanywishes.itch.io/joinlogic" className="btn btn-outline-success">Join Logic</a>
+                            <a href="https://grantyoumanywishes.itch.io/joinlogic" className="btn btn-outline-success" target='_blank' rel='noopener noreferrer'>Join Logic</a>
             </div>  
           </div>
           </div>
         <div className="game-container">
           <div className="">
-                <img src={bubbleAttack} className="card-img-top bubbleAttack img-fluid" alt="..."/>
+                <a href='https://michaelyount.itch.io/wind-whirl' target='_blank' rel='noopener noreferrer'> <img src={bubbleAttack} className="card-img-top bubbleAttack img-fluid" alt="..."/></a>
             <div className="">
               <p className="card-text-games">Video game development has many facets, 
               <br/>the work is never ending and there are more tasks that seem to build every day.
